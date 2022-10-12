8103c1bd0620e9c80bb0b45334b0cd9718cdf97b
Harish Sharma <hakershsharma@gmail.com>
2022-10-12 16:10:03 +0530

    String in Python
    Uploading the String in python session-code.

---
+++ String.py
@@ -0,0 +1,53 @@
+# To put the single line comment
+
+'''
+We can put the multi-line
+comment
+'''
+
+#Python is case sensitive language
+# How to initialize the string in variable.
+from email._header_value_parser import quote_string
+
+a = "Baba"
+print(type(a))
+print(a)
+
+#How to concatenate the string
+
+print("Hi "+a) #Hi Baba
+
+#How to take String-type input from User
+
+'''ToTakeTheInput = input("Please Enter your name: ")
+print(ToTakeTheInput)
+'''
+a = "Learn with Baba"
+A = "Learn with Harish"
+
+print(a)
+print(A)
+
+#Escape characters
+
+Var1 = "RuleBuilder\t depends on conditions."
+print(Var1)
+
+var1 = "Its a different\nstring"
+print(var1)
+
+#Can pass a quote in string
+
+QuoteString = "Mr. Atal Bihari was an Indian Prime-Minister, He once said -\"Freedom is incomplete without social justice\". "
+print(QuoteString)
+
+QuoteString1 = "Mr. Atal Bihari was an Indian Prime-Minister, He once said - 'Freedom is incomplete without social justice'. "
+print((QuoteString1))
+
+#Initializing the Multiline string
+
+MultilineString = ' String \
+is \
+Multi-Line'
+
+print(MultilineString)
