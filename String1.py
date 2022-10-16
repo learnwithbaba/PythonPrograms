8048623fcfa8dd0ece4ca055e18927710fd87a60
Confuse Coder <hakershsharma@gmail.com>
2022-10-16 15:48:08 +0530

    python code file to understand Type language
    python code file to understand Type language and ppt for same

---
+++ String1.py
@@ -0,0 +1,13 @@
+#Python is dynamic typed language
+
+age = 'twenty-seven' #string type
+print(type(age)) #<str>
+
+age = 27 #int type
+print(type(age))
+
+#Strongly typed language
+
+Name = 'Harish'
+
+print(Name+age)
